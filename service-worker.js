2fa4106c9e7c81b6b0ef9b6ff02eb3806574a8e2
4cGTDXFxAYK6 <species05618@gmail.com>
2026-03-08 11:54:13 +0900

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "sudoku-cache-v1";
+const FILES = [
+  "index.html",
+  "style.css",
+  "script.js",
+  "manifest.json",
+  "icons/icon-192.png",
+  "icons/icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
